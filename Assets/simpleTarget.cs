d253a450e6a06b85b40af506468de8f94bd0eed9
XabierUrrutia <xabier.urrutia@opendeusto.es>
2024-05-10 09:34:48 +0200

    terminado

--- Assets/simpleTarget.cs
+++ Assets/simpleTarget.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class simpleTarget : MonoBehaviour
 {
+    public Transform[] goals;
+    private NavMeshAgent agent;
+    private int count;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        count = 0;
+        agent.SetDestination(goals[count].position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("target number" + count);
+        if (agent.remainingDistance <= 0)
+        {
+            if (count >= goals.Length - 1)
+            {
+                count = 0;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        agent.SetDestination(goals[count].position);
+
     }
 }
